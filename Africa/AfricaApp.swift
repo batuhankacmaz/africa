35f795b5f0fe0303b1307403cbabc30831091f81
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-02-24 10:29:45 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Batuhan Kacmaz on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
