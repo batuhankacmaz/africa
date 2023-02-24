5f51112fe9470c77e2b708b6fa48f75842d2bc4b
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-02-24 10:50:14 +0300

    add assets and initial project structure

---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Batuhan Kacmaz on 24.02.2023.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video List")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
